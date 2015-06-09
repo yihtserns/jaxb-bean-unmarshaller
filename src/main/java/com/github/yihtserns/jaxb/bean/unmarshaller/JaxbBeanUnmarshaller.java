7a1a4cab4a024756da508a09a7c427fa43b2ad0e
yihtserns <sim_yihtsern@yahoo.com>
2015-06-09 16:47:38 +0800

    Move temporary maps into another class so can be discarded when done.

--- src/main/java/com/github/yihtserns/jaxb/bean/unmarshaller/JaxbBeanUnmarshaller.java
+++ src/main/java/com/github/yihtserns/jaxb/bean/unmarshaller/JaxbBeanUnmarshaller.java
@@ -16,6 +16,7 @@
 package com.github.yihtserns.jaxb.bean.unmarshaller;
 
 import com.github.yihtserns.jaxb.bean.unmarshaller.Unmarshaller.InitializableUnmarshaller;
+import com.github.yihtserns.jaxb.bean.unmarshaller.Unmarshaller.Provider.Handler;
 import java.beans.Introspector;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -29,19 +30,15 @@ import org.w3c.dom.Element;
  *
  * @author yihtserns
  */
-public class JaxbBeanUnmarshaller implements Unmarshaller.Provider {
+public class JaxbBeanUnmarshaller {
 
-    private Map<String, Unmarshaller> globalName2Unmarshaller = new HashMap<String, Unmarshaller>();
-    private Map<Class<?>, String> globalType2Name = new HashMap<Class<?>, String>();
-    private Map<Class<?>, InitializableUnmarshaller> type2Unmarshaller
-            = new HashMap<Class<?>, InitializableUnmarshaller>();
-    private Map<Class<?>, InitializableUnmarshaller> type2InitializedUnmarshaller
-            = new HashMap<Class<?>, InitializableUnmarshaller>();
+    private Map<String, Unmarshaller> globalName2Unmarshaller;
 
     /**
      * @see #newInstance(java.lang.Class...)
      */
-    private JaxbBeanUnmarshaller() {
+    private JaxbBeanUnmarshaller(Map<String, Unmarshaller> globalName2Unmarshaller) {
+        this.globalName2Unmarshaller = globalName2Unmarshaller;
     }
 
     public Object unmarshal(Element element) throws Exception {
@@ -51,74 +48,84 @@ public class JaxbBeanUnmarshaller implements Unmarshaller.Provider {
         return unmarshaller.unmarshal(element);
     }
 
-    private void addGlobalType(Class<?> type) throws Exception {
-        String elementName = resolveRootElementName(type);
-        Unmarshaller unmarshaller = getUnmarshallerForType(type);
-
-        globalName2Unmarshaller.put(elementName, unmarshaller);
-        globalType2Name.put(type, elementName);
-    }
-
-    @Override
-    public Unmarshaller getUnmarshallerForType(Class<?> type) throws Exception {
-        if (type2InitializedUnmarshaller.containsKey(type)) {
-            return type2InitializedUnmarshaller.get(type);
-        }
-        if (type2Unmarshaller.containsKey(type)) {
-            return type2Unmarshaller.get(type);
-        }
-        if (type == String.class) {
-            return StringUnmarshaller.INSTANCE;
-        }
-
-        InitializableUnmarshaller unmarshaller = new BeanUnmarshaller(type.getDeclaredConstructor());
-        type2Unmarshaller.put(type, unmarshaller);
-
-        return unmarshaller;
-    }
-
-    @Override
-    public void forGlobalUnmarshallerCompatibleWith(Class<?> type, Handler handler) {
-        for (Entry<Class<?>, String> entry : globalType2Name.entrySet()) {
-            Class<?> globalType = entry.getKey();
-            String globalName = entry.getValue();
-
-            if (!type.isAssignableFrom(globalType)) {
-                continue;
-            }
-            Unmarshaller unmarshaller = globalName2Unmarshaller.get(globalName);
-            handler.handle(globalName, unmarshaller);
-        }
-    }
-
-    private void init() throws Exception {
-        while (!type2Unmarshaller.isEmpty()) {
-            Collection<InitializableUnmarshaller> toBeInitialized = new ArrayList(type2Unmarshaller.values());
-            type2InitializedUnmarshaller.putAll(type2Unmarshaller);
-            type2Unmarshaller.clear();
-
-            for (InitializableUnmarshaller unmarshaller : toBeInitialized) {
-                unmarshaller.init(this);
-            }
-        }
-    }
-
     public static JaxbBeanUnmarshaller newInstance(Class<?>... types) throws Exception {
-        JaxbBeanUnmarshaller jaxbBeanUnmarshaller = new JaxbBeanUnmarshaller();
+        Builder builder = new Builder();
         for (Class<?> type : types) {
-            jaxbBeanUnmarshaller.addGlobalType(type);
+            builder.addGlobalType(type);
         }
-        jaxbBeanUnmarshaller.init();
+        builder.init();
 
-        return jaxbBeanUnmarshaller;
+        return new JaxbBeanUnmarshaller(builder.globalName2Unmarshaller);
     }
 
-    private static String resolveRootElementName(Class<?> type) {
-        XmlRootElement xmlRootElement = type.getAnnotation(XmlRootElement.class);
-        String name = xmlRootElement.name();
-        if (name.equals(BeanUnmarshaller.AUTO_GENERATED_NAME)) {
-            name = Introspector.decapitalize(type.getSimpleName());
+    private static final class Builder implements Unmarshaller.Provider {
+
+        private Map<String, Unmarshaller> globalName2Unmarshaller = new HashMap<String, Unmarshaller>();
+        private Map<Class<?>, String> globalType2Name = new HashMap<Class<?>, String>();
+        private Map<Class<?>, InitializableUnmarshaller> type2Unmarshaller
+                = new HashMap<Class<?>, InitializableUnmarshaller>();
+        private Map<Class<?>, InitializableUnmarshaller> type2InitializedUnmarshaller
+                = new HashMap<Class<?>, InitializableUnmarshaller>();
+
+        public void init() throws Exception {
+            while (!type2Unmarshaller.isEmpty()) {
+                Collection<InitializableUnmarshaller> toBeInitialized = new ArrayList(type2Unmarshaller.values());
+                type2InitializedUnmarshaller.putAll(type2Unmarshaller);
+                type2Unmarshaller.clear();
+
+                for (InitializableUnmarshaller unmarshaller : toBeInitialized) {
+                    unmarshaller.init(this);
+                }
+            }
         }
-        return name;
+
+        public void addGlobalType(Class<?> type) throws Exception {
+            String elementName = resolveRootElementName(type);
+            Unmarshaller unmarshaller = getUnmarshallerForType(type);
+
+            globalName2Unmarshaller.put(elementName, unmarshaller);
+            globalType2Name.put(type, elementName);
+        }
+
+        @Override
+        public Unmarshaller getUnmarshallerForType(Class<?> type) throws Exception {
+            if (type2InitializedUnmarshaller.containsKey(type)) {
+                return type2InitializedUnmarshaller.get(type);
+            }
+            if (type2Unmarshaller.containsKey(type)) {
+                return type2Unmarshaller.get(type);
+            }
+            if (type == String.class) {
+                return StringUnmarshaller.INSTANCE;
+            }
+
+            InitializableUnmarshaller unmarshaller = new BeanUnmarshaller(type.getDeclaredConstructor());
+            type2Unmarshaller.put(type, unmarshaller);
+
+            return unmarshaller;
+        }
+
+        @Override
+        public void forGlobalUnmarshallerCompatibleWith(Class<?> type, Handler handler) {
+            for (Entry<Class<?>, String> entry : globalType2Name.entrySet()) {
+                Class<?> globalType = entry.getKey();
+                String globalName = entry.getValue();
+
+                if (!type.isAssignableFrom(globalType)) {
+                    continue;
+                }
+                Unmarshaller unmarshaller = globalName2Unmarshaller.get(globalName);
+                handler.handle(globalName, unmarshaller);
+            }
+        }
+
+        private String resolveRootElementName(Class<?> type) {
+            XmlRootElement xmlRootElement = type.getAnnotation(XmlRootElement.class);
+            String name = xmlRootElement.name();
+            if (name.equals(BeanUnmarshaller.AUTO_GENERATED_NAME)) {
+                name = Introspector.decapitalize(type.getSimpleName());
+            }
+            return name;
+        }
     }
 }
